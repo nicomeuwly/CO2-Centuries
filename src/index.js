383690faf42bda8363d30e7600149e5e91c92c34
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-04-03 10:57:51 +0200

    events allert

--- src/index.js
+++ src/index.js
@@ -49,11 +49,8 @@ d3.xml('../ressources/infos.svg').then(
     infosPict.node().appendChild(data.documentElement)
   }
 )
-let positionPict
-d3.xml("../ressources/position.svg").then((data) => {
-  positionPict = data.documentElement
-  // console.log(positionPict)
-})
+
+const positionPict = "M50,12.55c-15.42,0-27.91,12.5-27.91,27.91c0,19.15,19.35,40.44,26.41,46.44c0.87,0.74,2.14,0.74,3.01,0  c7.06-6,26.41-27.29,26.41-46.44C77.91,25.04,65.42,12.55,50,12.55z M50,58.1c-9.27,0-16.78-7.51-16.78-16.78  c0-9.27,7.51-16.78,16.78-16.78s16.78,7.51,16.78,16.78C66.78,50.59,59.27,58.1,50,58.1z"
 
 
 const displayCover = (inf) => {
@@ -145,30 +142,45 @@ d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(
 );
 
 const placePointer = (event) => {
-  // Change the old current pointer clas for color
   if (document.querySelector('#p-' + event.id)) return
   d3.select('.current').attr("class", 'old')
   const coordinates = [event.long, event.lat]
-  // Insert new pointer
-  // d3.xml("../ressources/position.svg").then((data) => {
-    svg.append('svg')
-      .attr('width', 30)
-      .attr('height', 30)
-      .attr("x", projection(coordinates)[0]-15)
-      .attr("y", projection(coordinates)[1]-25)
-      .attr("id", "p-" + event.id)
-      .attr("class", "current")
-      .on("click", function () {
-        displayCover(event.text);
-      })
-      .node()
-      .appendChild(positionPict)
+
+  svg.append('svg')
+    .attr('width', 30)
+    .attr('height', 30)
+    .attr("x", projection(coordinates)[0]-15)
+    .attr("y", projection(coordinates)[1]-25)
+    .attr("id", "p-" + event.id)
+    .attr("class", "current")
+    .on("click", function () {
+      displayCover(event.text)
+    })
+    .append('path')
+    .attr('d', positionPict)
+    .attr("transform", "scale(0.3)")
+
+  const messageCont = document.querySelector('.message-cont')
+
+  const message = `<div id=ms-${event.id} class="message" data-active="true">
+    <h2>${event.title}</h2>
+    <p>${event.date}</p>
+  </div>`
+
+  messageCont.insertAdjacentHTML('afterbegin', message)
+  
+  document.querySelector(`#ms-${event.id}`).addEventListener('click', function() {
+    displayCover(event.text)
+  })
+
+  setTimeout(() => {
+    document.querySelector(`#ms-${event.id}`).dataset.active = "false"
+  }, 5000);
+  
+
 }
-
-
 // ########################################### ____ GET COLOR FROM CO2 
 const generateColor = (val) => {
-  
   let color
 
   if (val < 0.25) {
