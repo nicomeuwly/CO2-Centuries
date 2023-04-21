acc92b901553d53825ade57d0b5728c2a5a9137a
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-04-21 11:38:58 +0200

    Preview

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 export default defineConfig ({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                secondPage: resolve(__dirname, 'new-view.html')
+            }
+        }
+    },
     publicDir: 'public',
     base: './'
 })
